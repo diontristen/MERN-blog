a9341b9f0dcee977dfcf2fe3ac5b18e4025c8e83
Dion Tristen Aguilar <53813863+diontristen@users.noreply.github.com>
2020-05-28 14:56:09 +0800

    Remove Mongo DB URI and Credentials

--- backend/middleware/middleware.env.js
+++ backend/middleware/middleware.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     SECRET_KEY: 'ajinamoto',
     PORT: 5000,
-    MONGO_URI: 'mongodb+srv://admin123:admin123@cluster0-zpsji.mongodb.net/test?retryWrites=true&w=majority'
+    MONGO_URI: 'MONGO URI'
 }
